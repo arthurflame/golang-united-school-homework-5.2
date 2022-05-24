5d33061842c0be3c8852cbab56fac6eb61f69101
arthurflame <artgerash@gmail.com>
2022-05-24 16:37:32 +0300

    working on a Put method

--- cache.go
+++ cache.go
@@ -1,23 +1,71 @@
-package cache
+package main
 
-import "time"
+import (
+	"fmt"
+)
+type Data struct {
+	value string
+	canExpire bool
+	expirationTime int
+}
+
 
 type Cache struct {
+	data []map[string]Data
+
+
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func main() {
+	cache := NewCache()
+
+	cache.Put("1", "1111111111111")
+	//cache.Put("2", "2222222222222")
+	//cache.Put("2", "3333333333333")
+	fmt.Println("cache:", cache)
+}
+
+func (c *Cache) Put(key, value string) {
+
+	data := Data{
+		value:          value,
+		canExpire:      false,
+		expirationTime: 0,
+	}
+
+
+	//c.data = append(c.data, map[string]Data{key:data})
+
+	//fmt.Println(c.folder[0], data)
+
+
+	fmt.Println("c.data", c.data)
+
+	for i := 0; i < len(c.data); i++ {
+		if v, ok := c.data[i][key]; !ok {
+			c.data = append(c.data, map[string]Data{key:data})
+			//fmt.Println(k)
+			//c.data[0]["1"] = Data{value: "wewewew"}
+		} else {
+			c.data[i][key] = Data{value: value}
+		}
+	}
 
 }
 
-func (receiver) Put(key, value string) {
-}
 
-func (receiver) Keys() []string {
-}
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
-}
+//func (c *Cache) Get(key string) (string, bool) {
+//
+//}
+//
+//
+//func (receiver) Keys() []string {
+//}
+//
+//func (receiver) PutTill(key, value string, deadline time.Time) {
+//}
